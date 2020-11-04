dd41a6a42ff8124c77ddd7d9f797e3edc5007778
shalu.mishra <shalu.mishra@del1-lhp-n70140.synapse.com>
2020-11-04 18:41:39 +0530

    my commit

---
+++ src/main/java/com/GradleAvro/SchemaDemo/SchemaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.GradleAvro.SchemaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GradleAvro/SchemaDemo/SchemaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GradleAvro.SchemaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
